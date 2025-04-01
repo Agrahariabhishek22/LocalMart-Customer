d836f88a5fe2e6443b8c9096a1ff450da9eae84a
MAHENDRA PRATAP VERMA <132124880+Mahendra2611@users.noreply.github.com>
2025-04-02 00:45:22 +0530

    Update useAPI.jsx

--- src/hooks/useAPI.jsx
+++ src/hooks/useAPI.jsx
@@ -9,8 +9,9 @@ const useAPI = () => {
         setLoading(true);
         setError(null);
        
-        //url = `https://shopsy-backend-one.vercel.app/${url}`
-        url = `http://localhost:3000/${url}`;
+      //  url = `https://shopsy-backend-one.vercel.app/${url}`
+        url = `https://shopsy-backend-production.up.railway.app/${url}`
+       // url = `http://localhost:3000/${url}`;
         console.log(url)
         try {
             const response = await axios({
